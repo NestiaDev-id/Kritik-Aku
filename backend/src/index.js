2e27b5adf47f7dca9ed0833d2a2af7a5be55a2ba
NestiaDev-id <yohanesdevano90@gmail.com>
2025-03-10 22:22:27 +0700

    connect monggo db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Backend API is running! 🚀");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
 });
